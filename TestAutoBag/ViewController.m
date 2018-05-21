be71f72d2d78e497b6acd01d5c40e7813613edd0
王时温 <yahua523@163.com>
2018-05-21 11:39:28 +0800

    添加项目代码

---
+++ TestAutoBag/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestAutoBag
+//
+//  Created by wangshiwen on 2018/5/13.
+//  Copyright © 2018年 yahua. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
